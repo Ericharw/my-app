f2aa5f0f37744376ed0f6bda952d1a7db1653e5a
Ericharw <ericharizki12@gmail.com>
2026-03-04 14:12:07 +0700

    add: redirect otomatis ke login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,25 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useRouter } from 'next/router' 
+import { useEffect, useState } from 'react' 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [isLogin, setIsLogin] = useState(false);
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (!isLogin) {
+      push("/auth/login");
+    }
+  }, [isLogin, push]);
+
+  if (!isLogin) return null;
+
   return (
     <div>
-    
       <h1>Praktikum Next.js Pages Router</h1> <br />
       <p>Mahasiswa D4 Pengembangan Web</p>
     </div>
