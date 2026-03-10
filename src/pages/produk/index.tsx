a0ce9cb4843fbed94f6b9378fad918ae452168fe
Ericharw <ericharizki12@gmail.com>
2026-03-11 00:32:33 +0700

    add: skleton loading dan animasi

--- src/pages/produk/index.tsx
+++ src/pages/produk/index.tsx
@@ -1,6 +1,10 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import TampilanProduk from "../views/produk";
+import useSWR from "swr";
+import fetcher from "../utils/swr/fetcher";
+
+// conts fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const kategori = () => {
   // const [isLogin, setIsLogin] = useState(false);
@@ -13,21 +17,11 @@ const kategori = () => {
   //   }
   // },[]);
 
-  useEffect(() => {
-    fetch("/api/produk")
-      .then((response) => response.json())
-      .then((responsedata) => {
-        setProducts(responsedata.data);
-        // console.log("Data produk:", responsedata.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching produk:", error);
-      });
-  }, []);
+  const { data, isLoading } = useSWR("/api/produk", fetcher);
 
   return (
     <div>
-      <TampilanProduk products={products} />
+      <TampilanProduk products={isLoading ? [] : data.data} />
     </div>
   );
 };
--- src/pages/views/produk/index.tsx
+++ src/pages/views/produk/index.tsx
@@ -1,44 +1,27 @@
-import styles from "../produk/produk.module.scss";
+import styles from "./produk.module.scss";
 
-type ProductType = {
-  id: string;
-  name: string;
-  price: number;
-  image: string;
-  category: string;
-};
-
-const TampilanProduk = ({ products }: { products: ProductType[] }) => {
+const TampilanProduk = ({ products }: { products: any[] }) => {
   return (
     <div className={styles.produk}>
       <h1 className={styles.produk__title}>Daftar Produk</h1>
       <div className={styles.produk__content}>
         {products.length > 0 ? (
-          <>
-            {products.map((products: ProductType) => (
-              <div key={products.id} className={styles.produk__content__item}>
-                <div className={styles.produk__content__item__image}>
-                  <img src={products.image} alt={products.name} width={200} />
-                </div>
-                <h4 className={styles.produk__content__item__name}>
-                  {products.name}
-                </h4>
-                <p className={styles.produk__content__item__category}>
-                  {products.category}
-                </p>
-                <p className={styles.produk__content__item__price}>
-                  Rp {products.price.toLocaleString()}
-                </p>
-              </div>
-            ))}
-          </>
+          products.map((product: any) => (
+            <div key={product.id} className={styles.produk__content__item}>
+              <img src={product.image} alt={product.name} className={styles.produk__content__item__image} />
+              <h4 className={styles.produk__content__item__name}>{product.name}</h4>
+              <p className={styles.produk__content__item__category}>{product.category}</p>
+              <p className={styles.produk__content__item__price}>Rp {product.price.toLocaleString()}</p>
+            </div>
+          ))
         ) : (
-          <div className={styles.produk__content__skeleton}>
-            <div className={styles.produk__content__skeleton__image}></div>
-            <div className={styles.produk__content__skeleton__name}></div>
-            <div className={styles.produk__content__skeleton__category}></div>
-            <div className={styles.produk__content__skeleton__price}></div>
-          </div>
+          [1, 2, 3, 4].map((i) => (
+            <div key={i} className={styles.produk__content__skeleton}>
+              <div className={styles.produk__content__skeleton_image} />
+              <div className={styles.produk__content__skeleton_name} />
+              <div className={styles.produk__content__skeleton_price} />
+            </div>
+          ))
         )}
       </div>
     </div>
