6063731db70a0f7a65871871f94e2440dccf5aa9
Ericharw <ericharizki12@gmail.com>
2026-04-08 13:12:04 +0700

    add: tombol login logout

--- src/components/layouts/navbar/index.tsx
+++ src/components/layouts/navbar/index.tsx
@@ -1,11 +1,29 @@
 import syles from './navbar.module.css';
+import { signIn, signOut, useSession } from "next-auth/react"
 
 const Navbar = () => {
+  const { data } = useSession();
+
   return (
     <div className={syles.navbar}>
       <div className="big">
         Navbar
       </div>
+      {data ? (
+        <button 
+          style={{ backgroundColor: 'white', color: 'black', padding: '5px 10px', cursor: 'pointer' }} 
+          onClick={() => signOut()}
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button 
+          style={{ backgroundColor: 'white', color: 'black', padding: '5px 10px', cursor: 'pointer' }} 
+          onClick={() => signIn()}
+        >
+          Sign In
+        </button>
+      )}
     </div>
   );
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -14,5 +14,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|auth/login|auth/register).*)',],
+  matcher: ['/produk', '/about'],
 };
