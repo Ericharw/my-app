1b213de9c83f888f93557e1a4b6823ef34d5aa99
Ericharw <ericharizki12@gmail.com>
2026-04-23 14:36:04 +0700

    add: antisipasi error deployment

--- src/pages/produk/[produk].tsx
+++ src/pages/produk/[produk].tsx
@@ -1,23 +1,8 @@
-import fetcher from "@/utils/swr/fetcher";
-import { useRouter } from "next/router";
-import useSWR from "swr";
 import DetailProduk from "../../views/DetailProduk";
 import { ProductType } from "@/types/Product.type";
-import { retrieveData, retrieveDataByID } from "@/utils/db/servicefirebase";
+// import { retrieveData, retrieveDataByID } from "@/utils/db/servicefirebase"; 
 
 const HalamanProduk = ({product}:{product: ProductType}) => {
-  // const Router = useRouter();
-  // const { query } = useRouter();
-  // const { data, error, isLoading } = useSWR(
-  //   `/api/products/${query.produk}`,
-  //   fetcher,
-  // );
-  // return (
-  //   <div>
-  //     <DetailProduk products={isLoading ? [] : data.data} />
-  //   </div>
-  // );
-
   return (
     <div>
       <DetailProduk products={product} />
@@ -26,34 +11,44 @@ const HalamanProduk = ({product}:{product: ProductType}) => {
 };
 
 export default HalamanProduk;
-// export async function getServerSideProps({ params }: { params: { produk: string } }) {
-//   ...
-// }
 
-export async function getStaticPaths() {
-  const products = await retrieveData("products");
-
-  if (!products) {
-    return { paths: [], fallback: 'blocking' };
-  }
-
-  const paths = products.map((product: { id: string }) => ({
-    params: { produk: product.id }
-  }));
-
-  return {
-    paths,
-    fallback: 'blocking'
-  }
-}
-
-export async function getStaticProps({params}:{params:{produk:string}}) {
-  const productData = await retrieveDataByID("products", params.produk);
-
+export async function getServerSideProps({ params }: { params: { produk: string } }) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/produk/${params?.produk}`);
+  const respone = await res.json();
+  
+  // console.log("Data produk yang diambil dari API:", respone);
+  
   return {
     props: {
-      product: productData || null,
+      product: respone.data, 
     },
-    revalidate: 10
-  }
+  };
 }
+
+// export async function getStaticPaths() {
+//   const products = await retrieveData("products");
+//
+//   if (!products) {
+//     return { paths: [], fallback: 'blocking' };
+//   }
+//
+//   const paths = products.map((product: { id: string }) => ({
+//     params: { produk: product.id }
+//   }));
+//
+//   return {
+//     paths,
+//     fallback: 'blocking'
+//   }
+// }
+
+// export async function getStaticProps({params}:{params:{produk:string}}) {
+//   const productData = await retrieveDataByID("products", params.produk);
+//
+//   return {
+//     props: {
+//       product: productData || null,
+//     },
+//     revalidate: 10
+//   }
+// }
